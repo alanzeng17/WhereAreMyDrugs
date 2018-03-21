d6f13d8fc93a6292e8f7fc5e18eb4f1da9aff34f
Alan Zeng <alanzeng17@gmail.com>
2018-03-20 23:29:43 -0500

    up to second page done

--- src/App.js
+++ src/App.js
@@ -2,66 +2,88 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-export class App extends React.Component {
+var flag1 = false;
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-
+      <div className="main">
+        <div className="startComp">
+          <Start />
+        </div>
       </div>
     );
   }
 }
-export class Body extends React.Component {
-  constructor(props) {
-    super(props);
+
+class Start extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      value: 0,
+    };
   }
+  
+  render() {
+    var ret1;
+    if (this.state.value == 0){
+      ret1 = (      
+      <div class="start">
+        <div class="outer">
+          <div class="middle">
+            <div class="inner">
+              <h1>ApotheCare</h1>
+              <h4>Find the appropriate medicine for you!</h4>
+              <button class="cool" onClick={() => ak(this)}> Start </button>          
+            </div>
+          </div>
+        </div>
+      </div>
+      );
+    }
+    else {
+      ret1 = (
+        <div class="outer">
+          <div class="middle">
+            <div class="inner">
+              <div class="ill">
+                <h2 class='label'> Please choose your illness: </h2>
+                <select>
+                  <option value="d">--Select--</option>
+                  <option value="1"> Flu </option>
+                </select>
+                <hr></hr>
+                <button class='cool'> Continue </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    return (ret1)
+  }
+}
+
+class Illness extends React.Component {
   render() {
     return (
-      <div className ='body'>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">MedPack</h1>
-        </header>
-        <h1>Is This Medication Covered By My Insurance?</h1>
-        <form method = "post" action = "">
-        <table>
-        <tr class = 'insurance'>
-          <td>
-              <label> Medical Insurance:
-              <select name = "rating">
-                  <option selected = "selected"> Blue Cross Blue Shield</option>
-            <option> eHealth </option>
-            <option> Humana </option>
-          </select>
-              </label>
-            </td>
-        </tr>
-        <tr class = 'problem'>
-          <td>
-              <label> Choose your illness or medical problem:
-              <select name = "rating">
-                  <option selected = "selected"> Cold</option>
-            <option> Fever </option>
-            <option> Strep Throat </option>
-            <option> Flue </option>
-          </select>
-              </label>
-            </td>
-          
-        </tr>
-        <tr class = 'buttons'>
-          <td>
-            <input type = "submit" value = "Enter" />
-            <input type = "reset" value = "Clear" />
-          </td>
-        </tr>
-        
-        </table>
-        </form>
-      </div>
-
+      <p>wooorking</p>
     );
   }
 }
 
-//export default Body;
+function ak(s) {
+  //do something
+  flag1 = true;
+  s.setState({value: 1})
+  /*           <div class="dropdown">
+            <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+            <div id="myDropdown" class="dropdown-content">
+              <input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()">
+                <a href="#at">At</a>
+              </input>
+            </div>
+          </div>*/
+  
+}
+
+export default App;

d945208ac8d751b0a921416c3b0a5b44184d869e
alanzeng17 <alanzeng17@gmail.com>
2018-04-05 14:45:32 -0500

    Got second trasition working + saved the form value

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import IllnessSelect from './IllnessSelect.js'
 
 var flag1 = false;
+var globalState = null;
+var illnessVal = null;
 class App extends React.Component {
   render() {
     return (
@@ -21,6 +25,7 @@ class Start extends React.Component {
     this.state = {
       value: 0,
     };
+    globalState = this;
   }
   
   render() {
@@ -33,57 +38,54 @@ class Start extends React.Component {
             <div class="inner">
               <h1>ApotheCare</h1>
               <h4>Find the appropriate medicine for you!</h4>
-              <button class="cool" onClick={() => ak(this)}> Start </button>          
+              <button class="cool" onClick={() => transition(1)}> Start </button>          
             </div>
           </div>
         </div>
       </div>
       );
     }
-    else {
+    else if (this.state.value == 1){
       ret1 = (
         <div class="outer">
           <div class="middle">
             <div class="inner">
               <div class="ill">
                 <h2 class='label'> Please choose your illness: </h2>
-                <select>
-                  <option value="d">--Select--</option>
-                  <option value="1"> Flu </option>
-                </select>
-                <hr></hr>
-                <button class='cool'> Continue </button>
+                  <div>
+                    <IllnessSelect />
+                  </div>
               </div>
             </div>
           </div>
         </div>
       );
     }
+    else if (this.state.value == 2) {
+      ret1 = (        
+      <div class="outer">
+        <div class="middle">
+          <div class="inner">
+            <p>{illnessVal}</p>
+          </div>
+        </div>
+      </div>
+      );
+    }
     return (ret1)
   }
 }
 
-class Illness extends React.Component {
-  render() {
-    return (
-      <p>wooorking</p>
-    );
-  }
+export const transition = (value)=>{
+  if (globalState != null)
+    globalState.setState({value: value});
+  else 
+    alert("ERROR");
 }
 
-function ak(s) {
-  //do something
-  flag1 = true;
-  s.setState({value: 1})
-  /*           <div class="dropdown">
-            <button onclick="myFunction()" class="dropbtn">Dropdown</button>
-            <div id="myDropdown" class="dropdown-content">
-              <input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()">
-                <a href="#at">At</a>
-              </input>
-            </div>
-          </div>*/
-  
+export const getIllnessVal = (value)=>{
+  illnessVal = value;
 }
+
 
 export default App;
---
+++ src/IllnessSelect.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './App.css';
+import './App.js';
+import {transition,getIllnessVal} from './App.js'
+class IllnessSelect extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        value: '-Select Value-'
+      };
+      this.handleChange = this.handleChange.bind(this);
+      this.handlePrint = this.handlePrint.bind(this);
+    }
+  
+     handlePrint() {
+      if (this.state.value) {
+        console.log(this.state.value);
+        alert(this.state.value);
+        transition(2);
+        getIllnessVal(this.state.value);
+      }
+      else {
+        alert("failure");
+      }
+    }
+  
+    handleChange(e) {
+      this.setState({ value: e.target.value });
+    }
+  
+    render() {
+      return (
+        <div>
+          <div>
+            <select class="dropdown" onChange={this.handleChange}>
+              <option value="xxx">-Select Illness-</option>
+              <option value="flu">Flu</option>
+              <option value="strep">Strep Throat</option>
+              <option value="mono">Mono</option>
+              <option value ="pink">Pink Eye</option>
+              <option value ="FBI">Food Bourne Illness</option>
+          </select>
+          </div>
+          <hr></hr>
+          <button class='cool' onClick={this.handlePrint}> Continue </button>
+        </div>
+      );
+    }
+  }
+  
+  export default IllnessSelect;
